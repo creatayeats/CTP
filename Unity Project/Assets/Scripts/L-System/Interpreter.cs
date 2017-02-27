e7b14beea333a9af89577ae2124145bc22add1d2
creatayeats <createayeats@googlemail.com>
2017-02-27 13:24:10 +0000

    LSystem
    L-System is in and working, needs tweaks to make it more of a game
    mechanic

--- Unity Project/Assets/Scripts/L-System/Interpreter.cs
+++ Unity Project/Assets/Scripts/L-System/Interpreter.cs
@@ -1,119 +1,96 @@
 ﻿using UnityEngine;
+using System.IO;
 using System.Collections;
 using System.Collections.Generic;
 
 public class Interpreter : MonoBehaviour
 {
 
-    public string interSting;//move to private
+    // public string interSting;//move to private
     public GameObject turtle;
     public GameObject trunk;
 
-    public float angle;
-    public float rotationOfTrunk;
-    public int mode; //cant pass enum into switch witout converting to int, so just have int instead
+    private float angle = 90;
+    private string inter;
 
     private static Stack<Vector3> thePosStack = new Stack<Vector3>();
     private static Stack<Quaternion> theRotStack = new Stack<Quaternion>();
-    private ReWriter reWrite;
+
 
 
     void Start()
     {
-        //get ReWriting script
-        reWrite = GetComponent<ReWriter>();
+        StreamReader sr = new StreamReader("dragon.txt");
+        inter = sr.ReadLine();
+        //iterations = int.Parse(sr.ReadLine());
+        //stochY = bool.Parse(sr.ReadLine());
+        //stochX = bool.Parse(sr.ReadLine());
+        //stochF = bool.Parse(sr.ReadLine());
+        //ruleX = sr.ReadLine();
+        //ruleF = sr.ReadLine();
+        //ruleY = sr.ReadLine();
+        //ruleBO = sr.ReadLine();
+        //ruleBC = sr.ReadLine();
+        //ruleN = sr.ReadLine();
+        //ruleP = sr.ReadLine();
+        //forceSetAngle(float.Parse(sr.ReadLine()));
+        Debug.Log(inter);
+        sr.Close();
+
+        centerTurtle();
     }
 
     void Update()
     {
         //if generate bool is true
-        if (reWrite.getGen())
+        if (Input.GetKeyDown("space"))
         {
-            //get the final string for interptiation
-            interSting = reWrite.getFinalString();
-            //place turtle in center of screen
-            centerTurtle();
             mainLoop();
-            //turn gen back off
-            reWrite.setGen(false);
         }
 
         //if reset key pressed 
-        if (Input.GetKeyDown("r"))
-        {
-            //delete all trunks 
-            foreach (GameObject T in GameObject.FindGameObjectsWithTag("Player"))
-            { Destroy(T); }
+        //if (Input.GetKeyDown("r"))
+        //{
+        //    //delet all trunks 
+        //    foreach (GameObject T in GameObject.FindGameObjectsWithTag("Player"))
+        //    { Destroy(T); }
 
-        }
+        //}
     }
     private void centerTurtle()
     {
         //places turtle in the middle of the screen, 20X closer than the far plain
-        turtle.transform.position = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width / 2, Screen.height / 2, Camera.main.farClipPlane / 20));
-        turtle.transform.rotation = Quaternion.LookRotation(Vector3.forward);
+        turtle.transform.position = new Vector3(0.5f, 0.5f, 0.0f);
+        //turtle.transform.rotation = Quaternion.LookRotation(Vector3.forward);
     }
 
     private void mainLoop()
     {
         //look at each character in the final string
-        foreach (char c in interSting)
+        foreach (char c in inter)
         {
             switch (c)
             {
+
                 case 'f':
                 case 'F':
-                    switch (mode)
-                    {
-                        case 1:
-                            F2D();
-                            break;
-                        case 2:
-                            FTree();
-                            //extra mode
-                            break;
-                        default:
-                            break;
-                    }
+                    print("forward");
+                    F2D();
                     break;
-
                 case '[':
                     OnStack();
                     break;
-
                 case ']':
                     OffStack();
                     break;
-
                 case '+':
-                    switch (mode)
-                    {
-                        case 1:
-                            P2D();
-                            break;
-                        case 2:
-                            PTree();
-                            //extra mode
-                            break;
-                        default:
-                            break;
-                    }
+                    print("turn +");
+                    PTree();
                     break;
-
                 case '-':
-                case '−':
-                    switch (mode)
-                    {
-                        case 1:
-                            N2D();
-                            break;
-                        case 2:
-                            NTree();
-                            //extra mode
-                            break;
-                        default:
-                            break;
-                    }
+                case '?':
+                    print("turn -");
+                    NTree();
                     break;
                 default:
                     print("default");
@@ -122,52 +99,27 @@ public class Interpreter : MonoBehaviour
         }
     }
 
+
     private void F2D()
     {
         //create trunk at turtles location and rotation
         Instantiate(trunk, turtle.transform.position, turtle.transform.rotation);
         //move turtle forward
-        turtle.transform.Translate(Vector3.up * 2);
+        turtle.transform.Translate(Vector3.up);
     }
 
-    private void P2D()
-    {
-        //angle right
-        turtle.transform.Rotate(new Vector3(0.0f, 0.0f, -angle));
-    }
-
-    private void N2D()
-    {
-        //angle left
-        turtle.transform.Rotate(new Vector3(0.0f, 0.0f, angle));
-    }
-
-    //3d tree specific functions
-
-    //comon functions
-    private void FTree()
-    {
-        //create trunk at turtles location and rotation
-        Instantiate(trunk, turtle.transform.position, turtle.transform.rotation);
-        //move turtle forward
-        turtle.transform.Translate(Vector3.up * 2);
-        //add rotation for simple 3d 
-        turtle.transform.Rotate(new Vector3(0f, rotationOfTrunk, 0f));
-    }
-
-    //varience in angle
     private void PTree()
     {
         //angle right with slight varience 
-        float tempAngle = Random.Range(angle - 5, angle + 5);
-        turtle.transform.Rotate(new Vector3(0.0f, 0.0f, -tempAngle));
+        // float tempAngle = Random.Range(angle - 5, angle + 5);
+        turtle.transform.Rotate(new Vector3(0.0f, 0.0f, angle));
     }
 
     private void NTree()
     {
         //angle left with slight varience
-        float tempAngle = Random.Range(angle - 5, angle + 5);
-        turtle.transform.Rotate(new Vector3(0.0f, 0.0f, tempAngle));
+        //float tempAngle = Random.Range(angle - 5, angle + 5);
+        turtle.transform.Rotate(new Vector3(0.0f, 0.0f, -angle));
     }
 
     private void OnStack()
