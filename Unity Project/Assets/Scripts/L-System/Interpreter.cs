87acb36d368a6003197e7f5524e06f8a3ec2298e
creatayeats <createayeats@googlemail.com>
2017-03-03 20:04:50 +0000

    LSystem coroutine
    Set up the l system to destroy over time and create over time
    - boundaries need to be set
    - no overlaying on top of each other

--- Unity Project/Assets/Scripts/L-System/Interpreter.cs
+++ Unity Project/Assets/Scripts/L-System/Interpreter.cs
@@ -8,65 +8,43 @@ public class Interpreter : MonoBehaviour
     public GameObject turtle;
     public GameObject line;
 
-    private int needsCorrecting = 0;
-    private float angle = 90;
+    private int needsCorrecting;
+    private float angle = 90.0f;
+    private float lifetime = 5.0f;
+    //private float boundary = 5.0f;
+    private float stopwatch;
+    public float spawnTime = 1.0f;
     private string inter;
-    private string tempInter;
-    private string rightCorrecter;
-    private string leftCorrecter;
 
     private static Stack<Vector3> thePosStack = new Stack<Vector3>();
     private static Stack<Quaternion> theRotStack = new Stack<Quaternion>();
 
-
+    //private IEnumerator coroutine;
 
     void Start()
     {
-        StreamReader rules = new StreamReader("dragon.txt");
-        inter = rules.ReadLine();
+        //initial prefab placement
+        inter = "f";
         Debug.Log(inter);
-        rules.Close();
 
-        StreamReader correcter1 = new StreamReader("left.txt");
-        leftCorrecter = correcter1.ReadLine();
-        Debug.Log(leftCorrecter);
-        correcter1.Close();
-
-        StreamReader correcter2 = new StreamReader("right.txt");
-        rightCorrecter = correcter2.ReadLine();
-        Debug.Log(rightCorrecter);
-        correcter2.Close();
-
-        //tempInter = tempInter.Replace(tempInter, inter);
-        Debug.Log(tempInter);
         centerTurtle();
     }
 
     void Update()
     {
-        //if generate bool is true
-        if (Input.GetKeyDown("space"))
-        {
-            mainLoop();
-        }
+        //timer for spawning the next instance
+        stopwatch += Time.deltaTime * 4;
 
-        //if reset key pressed
-        if (Input.GetKeyDown("r"))
+        if (stopwatch >= spawnTime)
         {
-            needsCorrecting = 1;
             stringCorrecter();
-            //mainLoop();
-            //delet all trunks 
-            //foreach (GameObject T in GameObject.FindGameObjectsWithTag("Player"))
-            //{ Destroy(T); }
-
+            stopwatch = 0.0f;
         }
     }
     private void centerTurtle()
     {
-        //places turtle in the middle of the screen, 20X closer than the far plain
+        //places turtle in the middle of the grid
         turtle.transform.position = new Vector3(0.5f, 0.5f, 0.0f);
-        //turtle.transform.rotation = Quaternion.LookRotation(Vector3.forward);
     }
 
     private void mainLoop()
@@ -81,12 +59,12 @@ public class Interpreter : MonoBehaviour
                 case 'F':
                     F2D();
                     break;
-                case '[':
-                    OnStack();
-                    break;
-                case ']':
-                    OffStack();
-                    break;
+                //case '[':
+                //    OnStack();
+                //    break;
+                //case ']':
+                //    OffStack();
+                //    break;
                 case '+':
                     PTree();
                     break;
@@ -103,38 +81,67 @@ public class Interpreter : MonoBehaviour
 
     private void stringCorrecter()
     {
-        if (needsCorrecting == 1)
+
+        int path = Random.Range(1, 6);
+
+        if (path == 1 || needsCorrecting == 1)
         {
-            inter = inter.Replace(inter, leftCorrecter);
+            inter = inter.Replace(inter, "f++");
             Debug.Log(inter);
-            needsCorrecting = 0;
+            path = 0;
             mainLoop();
 
-            inter = inter.Replace(inter, "fff");
+            if (needsCorrecting == 1)
+            {
+                //if the path needs correcting
+                inter = inter.Replace(inter, "ff");
+            }
         }
-        if (needsCorrecting == 2)
+        if (path == 2 || needsCorrecting == 2)
         {
-            inter = inter.Replace(inter, rightCorrecter);
+            inter = inter.Replace(inter, "f--");
             Debug.Log(inter);
-            needsCorrecting = 0;
+            path = 0;
             mainLoop();
 
-            inter = inter.Replace(inter, "fff");
+            if (needsCorrecting == 2)
+            {
+                //if the path needs correcting
+                inter = inter.Replace(inter, "ff");
+            }
         }
-        //if (needsCorrecting == 0)
-        //{
-        //    inter = inter.Replace(inter, tempInter);
-        //    Debug.Log(inter);
-        //}
+            if (path == 3)
+        {
+            inter = inter.Replace(inter, "f+");
+            Debug.Log(inter);
+            path = 0;
+            mainLoop();
+        }
+        if (path == 4)
+        {
+            inter = inter.Replace(inter, "f-");
+            Debug.Log(inter);
+            path = 0;
+            mainLoop();
+        }
+        if (path == 5)
+        {
+            inter = inter.Replace(inter, "ff");
+            Debug.Log(inter);
+            path = 0;
+            mainLoop();
+        }
     }
-
 
     private void F2D()
     {
         //create trunk at turtles location and rotation
-        Instantiate(line, turtle.transform.position, turtle.transform.rotation);
+        GameObject clone = (GameObject)Instantiate(line, turtle.transform.position, turtle.transform.rotation);
         //move turtle forward
         turtle.transform.Translate(Vector3.up);
+
+        //boundaryCheck(clone);
+        destroyEnds(clone);
     }
 
     private void PTree()
@@ -149,22 +156,36 @@ public class Interpreter : MonoBehaviour
         turtle.transform.Rotate(new Vector3(0.0f, 0.0f, -angle));
     }
 
-    private void OnStack()
-    { //place turtles position into the position stack
-        Vector3 tempPos = turtle.transform.position;
-        thePosStack.Push(tempPos);
-        //place turtles rotation into the rotation stack
-        Quaternion tempRot = turtle.transform.rotation;
-        theRotStack.Push(tempRot);
+    //void OnTriggerEnter2D(Collider2D col)
+    //{
+    //    if (col.gameObject.tag == "Boundary")
+    //    {
+    //        needsCorrecting = 1;
+    //        print("Correcting..");
+    //    }
+    //}
+
+    private void destroyEnds(GameObject clone)
+    {
+        Destroy(clone, lifetime);
     }
 
-    private void OffStack()
-    {
-        // move turtle to position on top of stack
-        turtle.transform.position = thePosStack.Pop();
-        //rotate turtle to rotation on stack
-        turtle.transform.rotation = theRotStack.Pop();
-    }
+    //private void OnStack()
+    //{ //place turtles position into the position stack
+    //    Vector3 tempPos = turtle.transform.position;
+    //    thePosStack.Push(tempPos);
+    //    //place turtles rotation into the rotation stack
+    //    Quaternion tempRot = turtle.transform.rotation;
+    //    theRotStack.Push(tempRot);
+    //}
+
+    //private void OffStack()
+    //{
+    //    // move turtle to position on top of stack
+    //    turtle.transform.position = thePosStack.Pop();
+    //    //rotate turtle to rotation on stack
+    //    turtle.transform.rotation = theRotStack.Pop();
+    //}
 }
 
 
